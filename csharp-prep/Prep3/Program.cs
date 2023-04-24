c81538b7cffb6d4f1ad6281e5a2d3f0af255fab8
davm07 <diegovasquezmejia1999@gmail.com>
2023-04-24 16:14:18 +0200

    Prep3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1,101);
+        int guessNumber = 0;
+        int contador = 0;
+        
+        
+        while (guessNumber != randomNumber)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessNumber = int.Parse(guess);
+            contador++;
+
+            if (guessNumber == randomNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {contador} times");
+                }
+
+            else if (guessNumber < randomNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+
+            else if (guessNumber > randomNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+        }
+
     }
 }
