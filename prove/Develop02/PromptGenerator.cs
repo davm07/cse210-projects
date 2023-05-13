5eada937509541b3af17121de9288873c76e62cd
davm07 <diegovasquezmejia1999@gmail.com>
2023-05-13 08:54:36 +0200

    more questions added

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -7,7 +7,14 @@ public class PromptGenerator
         "What was the best part of my day?", 
         "How did I see the hand of the Lord in my life today?", 
         "What was the strongest emotion I felt today?", 
-        "If I had one thing I could do over today, what would it be?"
+        "What was the most challenging thing I faced today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What am I grateful for today?",
+        "What did I do today that I am proud of?",
+        "What did I learn today?",
+        "What are some things I would like to do differently tomorrow?",
+        "What did I do to help others today?",
+        "What were some unexpected events that took place today?"
     };
 
     public string SelectPrompt() 
