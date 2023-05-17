6886ca2d7cabe8d2188a0f4aedb6910e31a9f8a9
davm07 <diegovasquezmejia1999@gmail.com>
2023-05-17 16:38:30 +0200

    tests

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,21 +1,28 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Person person1 = new Person();
-        person1._name = "Luis";
-        person1._lastname = "Pardo";
+        // Person person1 = new Person();
+        // person1._name = "Luis";
+        // person1._lastname = "Pardo";
 
-        Person person2 = new Person();
-        person2._name = "Diego";
-        person2._lastname = "Vasquez";
+        // Person person2 = new Person();
+        // person2._name = "Diego";
+        // person2._lastname = "Vasquez";
 
-        Group group1 = new Group();
-        group1._groupName = "Perrones";
-        group1._people.Add(person1);
-        group1._people.Add(person2);
-        group1.DisplayGroup();
+        // Group group1 = new Group();
+        // group1._groupName = "Perrones";
+        // group1._people.Add(person1);
+        // group1._people.Add(person2);
+        // group1.DisplayGroup();
+
+        Console.WriteLine("AAA");
+        Console.ReadLine();
+        
+        // This will clear the console
+        Console.Clear();
+        
+        // This will show "BBB" in the console where "AAA" used to be
+        Console.WriteLine("BBB");
     }
 }
