d970bbc07c4302ce7e16cff8816b4e6d0a1b6b8c
davm07 <diegovasquezmejia1999@gmail.com>
2023-05-27 22:37:26 +0200

    Complete core requirements

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,19 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Word word1 = new Word("Diego");
-        // Console.WriteLine(word1.DisplayText());
-
-        // Reference reference = new Reference("Proverbs", 3, 5, 6);
-        // Console.WriteLine(reference.MakeReference());
-
         string sentence1 = "Trust in the Lord with all thine heart; and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
         Reference reference1 = new Reference("Proverbs", 3, 5, 6);
 
-        List<Scripture> scriptures = new();
-        scriptures.Add(new(sentence1, reference1));
-        Scripture myscripture = scriptures[0];
+        // List<Scripture> scriptures = new();
+        // scriptures.Add(new(sentence1, reference1));
+        // Scripture myscripture = scriptures[0];
         // Console.WriteLine(myscripture.MakeScripture());
+        Scripture myscripture = new(sentence1, reference1);
         
         string userChoice = "";
         // bool hidden = false;
@@ -25,12 +20,12 @@ class Program
         {   
             Console.Clear();
             Console.WriteLine(myscripture.MakeScripture());
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue or type \"quit\" to finish");
             if(myscripture.IsCompletlyHidden)
             {
                 break;
             }
-            Console.WriteLine();
-            Console.WriteLine("Press enter to continue or type \"quit\" to finish");
             myscripture.HideWords();
             userChoice = Console.ReadLine();
         }
