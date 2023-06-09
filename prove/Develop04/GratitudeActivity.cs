2759bfbaee6029bf9c4998475b3a779003a31de8
davm07 <diegovasquezmejia1999@gmail.com>
2023-06-09 21:20:26 +0200

    Create and add Gratitude Activity class

---
+++ prove/Develop04/GratitudeActivity.cs
@@ -0,0 +1,37 @@
+using System;
+
+public class GratitudeActivity : Activity
+{   
+    private string[] _gratitudePrompts = {"I'm grateful for three things I hear:", "I'm grateful for three things I see:", "I'm grateful for three things I smell:", "I'm grateful for these three things I taste:", "I'm grateful for these three friends:", "I'm grateful for these three things in my home:", "I'm grateful for these three things I've learned:", "I'm grateful for three acts of kindness I have received:", "I'm grateful for these three opportunities I have been given:"};
+    public GratitudeActivity()
+    {
+        _actName = "Gratitude Activity";
+        _actDescription = "This activity will help you think about the things in your life for which you are grateful, followed by writing down what you think.";
+    }
+
+    private void DisplayPrompt()
+    {
+        Console.WriteLine($"\n{SelectRandomPrompt(_gratitudePrompts)}");
+    }
+    public void RenderActivity()
+    {
+        Console.WriteLine("\nYou will have prompts to help you think the thing you are grateful for.");
+        Console.Write("You may begin in: ");
+        DisplayCountdown(3);
+        Console.WriteLine();
+        DateTime starTime = DateTime.Now;
+        DateTime endTime = starTime.AddSeconds(_actDuration);
+
+        while(DateTime.Now < endTime)
+        {   
+            int count = 0;
+            DisplayPrompt();
+            do
+            {   
+                Console.Write($"{count + 1}. ");
+                Console.ReadLine();
+                count++;
+            } while (count < 3);
+        }
+    }
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -10,13 +10,14 @@ class Program
     {
         Console.Clear();
         int option = 0;
-        while (option != 4)
+        while (option != 5)
         {   
             Console.WriteLine("Menu Options:");
             Console.WriteLine(Indent(4) + "1. Start breathing activity");
             Console.WriteLine(Indent(4) + "2. Start reflecting activity");
             Console.WriteLine(Indent(4) + "3. Start listing activity");
-            Console.WriteLine(Indent(4) + "4. Quit");
+            Console.WriteLine(Indent(4) + "4. Start gratitude activity");
+            Console.WriteLine(Indent(4) + "5. Quit");
             Console.Write("Select a choice from the menu: ");
             try
             {
@@ -49,6 +50,14 @@ class Program
                         Console.Clear();
                         break;
                     case 4:
+                        Console.Clear();
+                        GratitudeActivity gratAct = new GratitudeActivity();
+                        gratAct.DisplayStartMsg();
+                        gratAct.RenderActivity();
+                        gratAct.DisplayEndMsg();
+                        Console.Clear();
+                        break;
+                    case 5:
                         Console.WriteLine("See you next time!");
                         break;
                     default:
