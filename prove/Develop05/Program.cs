e3d2dee7f9cd92c15192ced5d5f1f9d2e1aab6f3
davm07 <diegovasquezmejia1999@gmail.com>
2023-06-15 18:47:44 +0200

    Create Menu

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -2,8 +2,24 @@ using System;
 
 class Program
 {
+    public static string Indent(int count)
+    {
+        return "".PadLeft(count);
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        int option = 0;
+        while (option != 6)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine(Indent(4) + "1. Create New Goal");
+            Console.WriteLine(Indent(4) + "2. List Goals");
+            Console.WriteLine(Indent(4) + "3. Save Goals");
+            Console.WriteLine(Indent(4) + "4. Load Goals");
+            Console.WriteLine(Indent(4) + "5. Record Events");
+            Console.WriteLine(Indent(4) + "6. Quit");
+            Console.Write("Select a choice from the menu: ");
+            option = int.Parse(Console.ReadLine());
+        }
     }
 }
