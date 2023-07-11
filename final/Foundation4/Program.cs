1da2ed4d58190ce701d10077c6529c78a7fa8153
davm07 <diegovasquezmejia1999@gmail.com>
2023-07-11 16:36:52 +0200

    Complete Foundation 4 project

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        Running running = new Running("03 Nov 2022", 30, 4.8);
+        running.GetSummary();
+
+        Swimming swimming = new Swimming("05 Oct 2023", 50, 15);
+        swimming.GetSummary();
+
+        Cycling cycling = new Cycling("11 Jul 2023", 40, 27);
+        cycling.GetSummary();
     }
 }
